b2f29191bb5d1e77cf007707ab945b576cd394bf
Gustavo <gusts92@gmail.com>
2020-11-06 21:50:21 -0300

    @home page part 2

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _loading = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,7 +17,7 @@ class _HomeState extends State<Home> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 50),
+            SizedBox(height: 85),
             Text(
               'Teachablemachine.com CNN',
               style: TextStyle(color: Color(0xFFEEDA28), fontSize: 15),
@@ -30,6 +32,67 @@ class _HomeState extends State<Home> {
               ),
             ),
             SizedBox(height: 40),
+            Center(
+              child: _loading
+                  ? Container(
+                      width: 280,
+                      child: Column(children: [
+                        Image.asset('assets/cat.png'),
+                        SizedBox(height: 50),
+                      ]),
+                    )
+                  : Container(),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 17,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE99600),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        'Take a photo',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width - 150,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 17,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFE99600),
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      child: Text(
+                        'Camera roll',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
